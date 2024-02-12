fd6af9d814d349ad634fda2aa53b90f02b7d7d39
Julia Holadava <juliaholadava@gmail.com>
2024-02-12 23:05:50 +0100

    feat: implement getAllUsersService function

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,5 +1,5 @@
 import { v4 as uuidv4 } from 'uuid';
-import * as user from '../models/user';
+import * as user from '../models/user.js';
 
 export const createUser = (
   username: string,
@@ -7,10 +7,16 @@ export const createUser = (
   hobbies: string[]
 ): user.User | Error => {
   if (!username || age <= 0 || !Array.isArray(hobbies)) {
-    return new Error('Invalid input data');
+    return new Error(
+      'Invalid input data. Input must have username, age, hobbies'
+    );
   }
 
   const id = uuidv4();
 
   return user.addUser({ id, username, age, hobbies });
+};
+
+export const getAllUsersService = () => {
+  return user.getAllUsers();
 };
