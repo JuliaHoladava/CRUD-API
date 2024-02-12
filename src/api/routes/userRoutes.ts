a581ddf979c1ffddda0422b661a96acd8cba3d0b
Julia Holadava <juliaholadava@gmail.com>
2024-02-12 23:04:16 +0100

    feat: add GET endpoint

--- src/api/routes/userRoutes.ts
+++ src/api/routes/userRoutes.ts
@@ -1,7 +1,11 @@
-import { handleCreateUserRequest } from '../controllers/userController';
+import {
+  handleCreateUserRequest,
+  handleGetUsersRequest,
+} from '../controllers/userController.js';
 
 export const userEndpoints = {
   '/api/users': {
     POST: handleCreateUserRequest,
+    GET: handleGetUsersRequest,
   },
 };
